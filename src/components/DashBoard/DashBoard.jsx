e86f7b1cffbd27ac8b38f3d46d6145fbc743e2a4
Mrunal Gadhave <mrunalg96@gmail.com>
2022-09-15 12:04:39 +0530

    Fundoo Notes React JS Project

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./DashBoard.css"
+import AppBar from '../AppBar/AppBar'
+// import AddNote from "../addNote/addNote";
+// import DisplayNote from "../DisplayNotes/DisplayNote";
+import Notes from '../Notes/notes';
+
+
+
+class dashboard extends React.Component {
+
+   render(){
+    return (
+        <div>
+          <AppBar />
+          <Notes />
+        </div>
+      )
+   }
+   
+}
+
+export default dashboard;
+
